710f84e67a8993c083e6b08f23db7e0604c7b69c
muahand-neyma <losylos084@gamil.com>
2024-06-08 09:37:45 -0700

    okyyyy

--- api/controlers/user.js
+++ api/controlers/user.js
@@ -233,7 +233,7 @@ export const getnotify = async(req,res,next) => {
   
   try{
  
-  const notify = await Notification.find({userId,_id:{$ne: user._id}}).sort({createdAt:-1})
+  const notify = await Notification.find({userId,id:{$ne: user.id}}).sort({createdAt:-1})
    
   res.json({posts:notify})
    
